27cd98b6cb3c1768201d65890b49c58a206b1246
Ugochukwu Charles <ugochukwucharles@Tech-Ugo.local>
2022-01-29 18:06:42 +0100

    Resized CloseSign

--- components/svg/CloseSign.js
+++ components/svg/CloseSign.js
@@ -1,10 +1,9 @@
-import React from 'react';
-
+import React from 'react'
 const CloseSign = ({ color= '#ffffff'}) => {
   return (
     <svg className="cursor-pointer" width="40" height="40" fill={color}>
-      <line x1="19" y1="0"  x2="19" y2="40" stroke={color} strokeWidth="3" />
-      <line x1="0" y1="19"  x2="37" y2="19" stroke={color} strokeWidth="3" />
+      <line x1="1" y1="1"  x2="19" y2="19" stroke={color} strokeWidth="3" />
+      <line x1="19" y1="1"  x2="1" y2="19" stroke={color} strokeWidth="3" />
     </svg>
   )
 };
