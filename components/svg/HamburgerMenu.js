7153f5e473e4d27e59f9a136ec146bb775ca13b0
Ugochukwu Charles <ugochukwucharles@Tech-Ugo.local>
2022-01-29 16:01:42 +0100

    Updated hamburger viewBox

--- components/svg/HamburgerMenu.js
+++ components/svg/HamburgerMenu.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const HamburgerMenu = ({ color = "#ffffff" }) => {
   return (
-    <svg viewBox="0 0 100 80" width="60" height="60" fill={color}>
+    <svg className="cursor-pointer" viewBox="0 0 50 80"  fill={color}>
       <rect width="50" height="5"></rect>
       <rect y="15" width="50" height="5"></rect>
       <rect y="30" width="50" height="5"></rect>
