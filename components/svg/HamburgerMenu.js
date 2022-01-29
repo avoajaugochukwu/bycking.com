7b55cc4f736b409111067a70ab366e76e74273f6
Ugochukwu Charles <ugochukwucharles@Tech-Ugo.local>
2022-01-29 15:56:50 +0100

    Reduced space between hamburger menu

--- components/svg/HamburgerMenu.js
+++ components/svg/HamburgerMenu.js
@@ -3,9 +3,9 @@ import React from "react";
 const HamburgerMenu = ({ color = "#ffffff" }) => {
   return (
     <svg viewBox="0 0 100 80" width="60" height="60" fill={color}>
-      <rect width="60" height="8"></rect>
-      <rect y="20" width="60" height="8"></rect>
-      <rect y="40" width="60" height="8"></rect>
+      <rect width="50" height="5"></rect>
+      <rect y="15" width="50" height="5"></rect>
+      <rect y="30" width="50" height="5"></rect>
     </svg>
   );
 };
